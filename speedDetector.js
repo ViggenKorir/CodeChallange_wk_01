b34dcbbe2d9f861c951f08ac918f6b4cdab0589f
Korir Emanuel <110737113+ViggenKorir@users.noreply.github.com>
2024-07-03 20:35:09 +0300

    Update speedDetector.js

--- speedDetector.js
+++ speedDetector.js
@@ -2,16 +2,16 @@
 //(challange 2)
 let carSpeed = prompt("Enter your speed");
 const speedLimit = 70;
-if (carSpeedpeed <= speedLimit && carSpeed >= 0) {
+if (carSpeed <= speedLimit && carSpeed >= 0) {
     console.log("OK");
 } else {
-        if (carSpeedpeed > 70) {
-            let points = Math.floor((carSpeedpeed - 70) / 5);
+        if (carSpeed > 70) {
+            let points = ((carSpeed - 70) / 5);
             console.log(points);
-        }
+      
         if (points > 12) {
             console.log("License Suspended");
         } else {
             console.log("Points", points);
         }
-    }
+    }}
