49550adee748851ae70bbf610104ce969617424f
Korir Emanuel <110737113+ViggenKorir@users.noreply.github.com>
2024-07-03 20:35:42 +0300

    Update studentGradeGenerator.js

--- studentGradeGenerator.js
+++ studentGradeGenerator.js
@@ -1,12 +1,12 @@
 // STUDENT GRADE GENERATOR
 // challange 1
 let marks = prompt("Enter your marks")
-marks <= 100 && marks >= 0;
-if (marks > 100)
-    console.log("Invalid marks")
-else if (marks < 0)
-    console.log("Invalid marks")
-function studentGrade(marks) {
+//validate if the marks are within the valid range
+if (marks >= 100 && marks < 0)
+    console("Invalid marks");
+  else
+    //Function to assign grades to students based on theirmarks
+  function studentGrade(marks) {
     if (marks > 79)
         console.log("A")
     else if (marks >= 60)
@@ -15,8 +15,8 @@ function studentGrade(marks) {
         console.log("C")
     else if (marks >= 40)
         console.log("D")
-    else (marks < 40)
+    else if(marks < 40)
     console.log("E")
-}
+    }
 
-console.log(studentGrade(marks))
+console.log(studentGrade(marks));
